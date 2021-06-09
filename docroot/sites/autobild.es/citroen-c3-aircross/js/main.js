4fbc559663e06079889c1ff6e7ca7854b24b133c
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2021-06-09 14:26:08 +0200

    Citroen

--- docroot/sites/autobild.es/citroen-c3-aircross/js/main.js
+++ docroot/sites/autobild.es/citroen-c3-aircross/js/main.js
@@ -1,15 +1,13 @@
 $( ".play" ).click(function(e) {
   e.preventDefault();
-  var ref = $(this).attr('src');
+  var ref = "https://www.youtube.com/embed/JEmeQ2m_iZA"
   $( ".modal-vi iframe" ).attr('src',ref);
   $("body").addClass('modal-visible');
-  console.log("video");
 });
 
 $( ".gallery" ).click(function(e) {
   e.preventDefault();
   $("body").addClass('modal-visible-gal');
-  console.log("gallery");
 
 });
 
@@ -18,6 +16,5 @@ $( ".close" ).click(function(e) {
   $("body").removeClass('modal-visible');
   $("body").removeClass('modal-visible-gal');
   $( ".modal-vi iframe" ).attr('src','');
-  console.log("cierro");
 
 });
