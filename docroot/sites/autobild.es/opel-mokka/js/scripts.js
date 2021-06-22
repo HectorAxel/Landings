e319595f598c0b19e22414a90bc511477d5cc1df
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2021-06-22 14:46:01 +0200

    Mokka

--- docroot/sites/autobild.es/opel-mokka/js/scripts.js
+++ docroot/sites/autobild.es/opel-mokka/js/scripts.js
@@ -8,23 +8,23 @@ $(".close").click(function(e) {
 
 
 
-$(".historys > .info").click(function(e) {
+$(".historys .contain-buttons > .info").click(function(e) {
   e.preventDefault();
   $(".popup, .one").css("display", "flex");
   $("html").css("overflow", "hidden");
 });
-$(".historys > .gallery").click(function(e) {
+$(".historys .contain-buttons > .gallery").click(function(e) {
   e.preventDefault();
   $(".popup, .two").css("display", "block");
   $("html").css("overflow", "hidden");
 });
-$(".historys > .interview").click(function() {
+$(".historys .contain-buttons > .interview").click(function() {
   $('.popup').css('background-image','url(img/back1.jpg)');
   $(".popup, .five").css("display", "flex");
   $("html").css("overflow", "hidden");
 });
 
-$(".historys > .video").click(function(e) {
+$(".historys .contain-buttons > .video").click(function(e) {
   e.preventDefault();
   $(".popup, .four").css("display", "block");
   $('#player').css("display", "block");
@@ -32,7 +32,7 @@ $(".historys > .video").click(function(e) {
 });
 
 
-$(".historys > .infos").click(function(e) {
+$(".historys .contain-buttons > .infos").click(function(e) {
   $('.popup').css('background-image','url(img/back2.jpg)');
   $(".popup, .three").css("display", "flex");
   $("html").css("overflow", "hidden");
