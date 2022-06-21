0e8c1246d84aa09c6576d3e2a650b11ccedd84fe
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2022-06-21 13:16:23 +0200

    iphone

---
+++ docroot/sites/businessinsider.es/iphone/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".scroll-graphic").scrollSwap({
+    dataImageAttribute: "image",
+    triggerFromTop: 0,
+    speed: 0.5,
+    noCSS: false,
+    spreadDistance: "75vh",
+    backgroundColor: "rgba(255,255,255,0.85)",
+    width: "50%",
+  });
+});
