abd25bda8208f0f89ae9045b41e452ccabb144b8
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2022-06-23 08:41:50 +0200

    Honor

--- docroot/sites/businessinsider.es/iphone/js/main.js
+++ docroot/sites/businessinsider.es/iphone/js/main.js
@@ -2,7 +2,7 @@ $(function () {
   $(".scroll-graphic").scrollSwap({
     dataImageAttribute: "image",
     triggerFromTop: 0,
-    speed: 0.5,
+    speed: 1,
     noCSS: false,
     spreadDistance: "75vh",
     backgroundColor: "rgba(255,255,255,0.85)",
