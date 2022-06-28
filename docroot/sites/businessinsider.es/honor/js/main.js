b44a171fd3cf25c300e2f19ee7d94cec935df16f
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2022-06-28 11:19:52 +0200

    Honor

--- docroot/sites/businessinsider.es/honor/js/main.js
+++ docroot/sites/businessinsider.es/honor/js/main.js
@@ -5,6 +5,9 @@ window.addEventListener(
       document.querySelector(".loader").classList.add("invisible");
     }, 1000);
     setTimeout(() => {
+      document.querySelector(".main-header").classList.add("invisible");
+    }, 1500);
+    setTimeout(() => {
       document.querySelector(".loader").remove();
     }, 2000);
   },
