b99193ab7b7cb971445e87a09702627103c0b7b9
Héctor de Lorenzo Saez <hector.delorenzo@HECTOR-DELORENZO.local>
2022-06-28 13:17:37 +0200

    delete

--- docroot/sites/businessinsider.es/bmw-next-gen/js/main.js
+++
@@ -1,6 +0,0 @@
-//el.dataset.bg
-document.querySelectorAll(".articles article").forEach((el, k) => {
-  el.style.background = `url(${el.dataset.bg}) no-repeat center ${
-    el.dataset.bgal || "center"
-  } / cover`;
-});
