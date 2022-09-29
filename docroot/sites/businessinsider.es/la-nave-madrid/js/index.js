a90fc7a28c1e5a6a1b0f72bce34c7ef7f4364d1b
Héctor de Lorenzo <hector.delorenzo@axelspringer.es>
2022-09-29 10:04:57 +0200

    Nave

--- docroot/sites/businessinsider.es/la-nave-madrid/js/index.js
+++ docroot/sites/businessinsider.es/la-nave-madrid/js/index.js
@@ -1,16 +1,19 @@
 $(".box.orange").click(function () {
+  $(this).addClass("visible");
   $(".group-photos .orange").addClass("visible");
-  $(".group-photos .yellow").removeClass("visible");
-  $(".group-photos .green").removeClass("visible");
+  $(".group-photos .yellow, .group-photos .green").removeClass("visible");
+  $(".box.green, box.yellow").removeClass("visible");
   console.log("hola");
 });
 $(".box.yellow").click(function () {
+  $(this).addClass("visible");
   $(".group-photos .yellow").addClass("visible");
-  $(".group-photos .green").removeClass("visible");
-  $(".group-photos .orange").removeClass("visible");
+  $(".group-photos .green, .group-photos .orange").removeClass("visible");
+  $(".box.orange, box.green").removeClass("visible");
 });
 $(".box.green").click(function () {
+  $(this).addClass("visible");
   $(".group-photos .green").addClass("visible");
-  $(".group-photos .yellow").removeClass("visible");
-  $(".group-photos .orange").removeClass("visible");
+  $(".group-photos .yellow, .group-photos .orange").removeClass("visible");
+  $(".box.orange, .box.yellow").removeClass("visible");
 });
