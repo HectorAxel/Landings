abdcf660dda276fc7cfea701695b9ef2b003f9a7
Héctor de Lorenzo <hector.delorenzo@axelspringer.es>
2022-09-29 10:51:50 +0200

    Nave

--- docroot/sites/businessinsider.es/la-nave-madrid/js/index.js
+++ docroot/sites/businessinsider.es/la-nave-madrid/js/index.js
@@ -17,3 +17,32 @@ $(".box.green").click(function () {
   $(".group-photos .yellow, .group-photos .orange").removeClass("visible");
   $(".box.orange, .box.yellow").removeClass("visible");
 });
+
+$(".scroll-1").click(function () {
+  $(".web-desktop").css("transform", "translate(-97vw, 0px)");
+  $(".web-desktop section.yellow p, .web-desktop .group-box").addClass(
+    "visible"
+  );
+});
+$(".scroll-2").click(function () {
+  $(".web-desktop").css("transform", "translate(-194vw, 0px)");
+});
+$(".scroll-3").click(function () {
+  $(".web-desktop").css("transform", "translate(-291vw, 0px)");
+});
+$(".scroll-4").click(function () {
+  $(".web-desktop").css("transform", "translate(-385vw, 0px)");
+});
+
+$(".scroll-pre-1").click(function () {
+  $(".web-desktop").css("transform", "translate(-291vw, 0px)");
+});
+$(".scroll-pre-2").click(function () {
+  $(".web-desktop").css("transform", "translate(-194vw, 0px)");
+});
+$(".scroll-pre-3").click(function () {
+  $(".web-desktop").css("transform", "translate(-97vw, 0px)");
+});
+$(".scroll-pre-4").click(function () {
+  $(".web-desktop").css("transform", "translate(0vw, 0px)");
+});
