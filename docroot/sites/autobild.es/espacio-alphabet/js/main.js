777d7c79414c20fc662c4a9e86fc9d31d75e5100
Héctor de Lorenzo <hector.delorenzo@axelspringer.es>
2022-12-01 15:06:29 +0100

    alphabet

---
+++ docroot/sites/autobild.es/espacio-alphabet/js/main.js
@@ -0,0 +1,23 @@
+$(".photos").click(function () {
+  $(".wrapper-popup").addClass("visible");
+  $("body").addClass("visible");
+});
+
+$(".close").click(function () {
+  $(".wrapper-popup").removeClass("visible");
+  $("body").removeClass("visible");
+});
+
+$(".select-buttons a").click(function (e) {
+  e.preventDefault();
+  var el = $(this).attr("data-href");
+  $(".select").removeClass("show");
+  $("#" + el).addClass("show");
+  if (el == "store") {
+    $(".selected").removeClass("selected");
+    $(".store").addClass("selected");
+  } else if (el == "valent") {
+    $(".selected").removeClass("selected");
+    $(".valent").addClass("selected");
+  }
+});
